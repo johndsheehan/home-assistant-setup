207538870868ebbc64af95b2e60b6b4de98a30a3
John D Sheehan <john.d.sheehan@gmail.com>
2018-01-12 16:54:05 +0000

    make mqtt more robust in a try/except block

--- utils/dht22/main.py
+++ utils/dht22/main.py
@@ -9,28 +9,38 @@ def sub_cb(topic, msg):
     print(msg)
 
 
-def mqtt_msg(topic, delay):
-    client = MQTTClient('esp8266', 'flatpi.lan', port=1883)
+def mqtt_msg(config):
+    host = config['host']
+    port = config['port']
+    mqtt_id = config['mqtt_id']
+    topic = config['topic']
+    delay = config['delay']
+
+    client = MQTTClient(mqtt_id, host, port=port)
     client.connect()
- 
+
     d = dht.DHT22(machine.Pin(4))
     try:
         '''required or a timeout error occurs !?'''
         d.measure()
     except:
-        pass
+        print('d.measure() failed')
 
     while True:
-        d.measure()
-        t = time.localtime()
+        try:
+            d.measure()
+            t = time.localtime()
 
-        timestamp = '{}{:02d}{:02d}{:02d}{:02d}{:02d}'.format(t[0], t[1], t[2], t[3], t[4], t[5])
-        temperature = d.temperature()
-        humidity = d.humidity()
+            timestamp = '{}{:02d}{:02d}{:02d}{:02d}{:02d}'.format(t[0], t[1], t[2], t[3], t[4], t[5])
+            temperature = d.temperature()
+            humidity = d.humidity()
 
-        m = '{{"tsp":"{}", "tmp":{:02.2f}, "hmd":{:02.2f}}}'.format(timestamp, temperature, humidity)
+            m = '{{"tsp":"{}", "tmp":{:02.2f}, "hmd":{:02.2f}}}'.format(timestamp, temperature, humidity)
 
-        client.publish(topic=topic, msg=m)
+            client.publish(topic=topic, msg=m)
+        except:
+            print('d.measure() failed')
+
         time.sleep(delay)
 
 
@@ -40,6 +50,6 @@ if __name__ == '__main__':
             import ujson
             cfg = ujson.loads(f.read())
     
-        mqtt_msg(cfg["topic"], cfg["delay"])
+        mqtt_msg(cfg['mqtt'])
     except:
         print('failed to start mqtt msg')
